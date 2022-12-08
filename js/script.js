b505597f9e4a2b4ed9dd4b78901d78c6cdcb3988
Jonasz Ja <jajonaszjj@gmail.com>
2022-12-08 10:12:59 +0100

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -14,14 +14,42 @@
     let htmlString = "";
 
     for (const task of tasks) {
-      htmlString += `<li>${task.content}</li>`;
+      htmlString += `<li 
+          ${task.done ? ' style="text-decoration: line-through"' : ""}
+        >
+          ${task.content}
+        </li>`;
     }
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
   };
 
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
+  };
+
   const init = () => {
     render();
+
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", onFormSubmit);
   };
 
   init();
